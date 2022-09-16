3e3d94eeffd30e55b9fff8ec9161cb0b7bc94ee8
bmaggiano <brandon.maggiano@gmail.com>
2022-09-16 14:44:48 -0700

    add generate password function

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,13 @@ const specialChar = ['!','@','#','$','%','^','&','*','?','>','<','~','+','=','('
 const numbers = ['1','2','3','4','5','6','7','8','9','0'];
 
 
+function generatePassword() {
 
+
+
+
+  
+}
 
 
 // Add event listener to generate button
