ce205b0c8412d17b69a5c20cc42368a5e643106f
bmaggiano <brandon.maggiano@gmail.com>
2022-09-16 14:46:00 -0700

    added characters prompt

--- assets/script.js
+++ assets/script.js
@@ -19,10 +19,17 @@ const numbers = ['1','2','3','4','5','6','7','8','9','0'];
 
 function generatePassword() {
 
+  var charLength = prompt("Please enter a character value between 8-128");
+  charLength = parseInt(charLength);
+
+  if (charLength < 8 || charLength > 128) {
+    alert('Not a valid entry, try again');
+    return;
+  } else {
+    alert("PERFECT! Now let's talk protection!")
+  }
 
 
-
-  
 }
 
 
