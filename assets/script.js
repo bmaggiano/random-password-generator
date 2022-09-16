f07d6b09411c5f798bfd08482ec4a8814619ee80
bmaggiano <brandon.maggiano@gmail.com>
2022-09-16 14:43:51 -0700

    Add password arrays

--- assets/script.js
+++ assets/script.js
@@ -10,5 +10,15 @@ function writePassword() {
 
 }
 
+//password arrays
+const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const specialChar = ['!','@','#','$','%','^','&','*','?','>','<','~','+','=','(',')'];
+const numbers = ['1','2','3','4','5','6','7','8','9','0'];
+
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
